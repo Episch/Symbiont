7c627ddbfdcddba5228b511ad5d86a1e178e045b
David Nagelschmidt <nagelschmidt>
2021-04-14 23:30:05 +0200

    [Feature] image warp perspective

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 import pytesseract
 import os
 
@@ -10,6 +10,7 @@ pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesse
 
 # load query image as compare default
 imQ = cv2.imread('sampleData/Query.jpg')
+h, w, c = imQ.shape
 
 # image detection
 orb = cv2.ORB_create(1000)
@@ -26,8 +27,15 @@ for j, y in enumerate(myPictureList):
     # lower the distance equals better that result
     matches.sort(key=lambda x: x.distance)
     good = matches[:int(len(matches) * (percent / 100))]
-    imageMatch = cv2.drawMatches(image, kp2, imQ, kp1, good[:100], None, flags=2)
-    cv2.imshow(y, imageMatch)
+    # imageMatch = cv2.drawMatches(image, kp2, imQ, kp1, good[:50], None, flags=2)
+    # cv2.imshow(y, imageMatch)
+    srcPoints = np.float32([kp2[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+    destinationPoints = np.float32([kp1[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+    M, _ = cv2.findHomography(srcPoints, destinationPoints, cv2.RANSAC, 5.0)
+    imageScan = cv2.warpPerspective(image, M, (w, h))
+    imageScan = cv2.resize(imageScan, (w // 3, h // 3))
+    cv2.imshow(y, imageScan)
 
 # out put image
 # cv2.imshow("Keypoints", imKp1)
