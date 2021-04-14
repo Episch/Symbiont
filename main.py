37ccedbbe2c8b860e2eccab29fcec82313c695ee
David Nagelschmidt <nagelschmidt>
2021-04-15 01:41:24 +0200

    [Feature] image to text

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import pytesseract
 import os
 
 # percent of the best compare matches
-percent = 25;
+percent = 25
+pixelThreshold = 160
+
+roi = [[(123, 65), (259, 76), 'text', 'Name'],
+       [(71, 214), (90, 224), 'box', 'Vehicletype']]
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
@@ -13,7 +17,7 @@ imQ = cv2.imread('sampleData/Query.jpg')
 h, w, c = imQ.shape
 
 # image detection
-orb = cv2.ORB_create(1000)
+orb = cv2.ORB_create(1100)
 kp1, des1 = orb.detectAndCompute(imQ, None)
 
 path = 'compareData'
@@ -32,11 +36,36 @@ for j, y in enumerate(myPictureList):
     srcPoints = np.float32([kp2[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
     destinationPoints = np.float32([kp1[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
 
-    M, _ = cv2.findHomography(srcPoints, destinationPoints, cv2.RANSAC, 5.0)
+    M, _ = cv2.findHomography(srcPoints, destinationPoints, cv2.RANSAC, 2.0)
     imageScan = cv2.warpPerspective(image, M, (w, h))
-    imageScan = cv2.resize(imageScan, (w // 3, h // 3))
-    cv2.imshow(y, imageScan)
+    # imageScan = cv2.resize(imageScan, (w // 3, h // 3))
+    # cv2.imshow(y, imageScan)
+    imageShow = imageScan.copy()
+    imageMask = np.zeros_like(imageShow)
 
+    myData = []
+    print(f' Extracting from {myPictureList[j]}')
+
+    for x, r in enumerate(roi):
+        cv2.rectangle(imageMask, ((r[0][0]), r[0][1]), ((r[1][0]), r[1][1]), (0, 255, 0), cv2.FILLED)
+        imageShow = cv2.addWeighted(imageShow, 0.99, imageMask, 0.1, 0)
+
+        imageCrop = imageScan[r[0][1]:r[1][1], r[0][0]:r[1][0]]
+        # cv2.imshow(str(x), imageCrop)
+
+        if r[2] == 'text':
+            print(f'{r[3]}: {pytesseract.image_to_string(imageCrop)}')
+            text = myData.append(pytesseract.image_to_string(imageCrop))
+        if r[2] == 'box':
+            imageGray = cv2.cvtColor(imageCrop, cv2.COLOR_BGR2GRAY)
+            imageThresh = cv2.threshold(imageGray, 170, 255, cv2.THRESH_BINARY_INV)[1]
+            totalPixels = cv2.countNonZero(imageThresh)
+            if totalPixels>pixelThreshold: totalPixels=1
+            else: totalPixels=0
+            print(f'{r[3]}: {totalPixels}')
+            myData.append(totalPixels)
+
+    cv2.imshow(y + "2", imageShow)
 # out put image
 # cv2.imshow("Keypoints", imKp1)
 # cv2.imshow("Output", imQ)
